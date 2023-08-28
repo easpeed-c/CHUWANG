d513683839422d388d92ca5f7d7febe45e2606be
WilfordfLethe <WilfordfLethe@gmail.com>
2023-08-28 09:25:57 +0800

    add lrc chesk for C

---
+++ 1_Workspace/5_Software/LRC/lrc.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+#include "stdint.h"
+
+uint8_t data[7];
+
+int main(void)
+{
+    uint8_t lrc = 0x01;
+    uint8_t sum = 0;
+    for (uint8_t i = 0; i < 6; i++)
+    {
+        if (i == 5)
+        {
+            scanf("%x", &data[i]);
+        }
+        else
+        {
+            scanf("%x-", &data[i]);
+        }
+    }
+    for (uint8_t i = 0; i < 6; i++)
+    {
+        sum += data[i];
+    }
+    lrc = (uint8_t)~sum + 1;
+    data[6] = lrc;
+    printf("lrc=%x", data[6]);
+    return 0;
+}
